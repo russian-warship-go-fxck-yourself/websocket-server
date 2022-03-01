655f9a0b62cfb71efaf1b16d4ab8581cc4123bd4
russian-warship-go-fxck-yourself <russian-warship-go-f-yourself@protonmail.com>
2022-03-01 03:03:36 +0100

    add target logs

--- src/index.js
+++ src/index.js
@@ -11,15 +11,16 @@ const io = new Server(httpServer, {
 });
 
 global._config = {
-  // statsIntervalTime: 1000 * 60 * 5,
   statsIntervalTime: 2000,
-  targetsIntervalTime: 5000,
+  targetsIntervalTime: 5 * 60 * 1000, // 5 min
+  maxTargetsLogLength: 5,
 };
 
 global._stats = {
   clientsCount: 0,
   webVisitorsCount: 0,
   currentTargets: ["https://www.1tv.com/"],
+  targetsLogs: [],
 };
 
 io.of("client").on("connection", (socket) => {
@@ -27,8 +28,20 @@ io.of("client").on("connection", (socket) => {
 
   emitTargets(socket);
 
+  socket.on('targets:log', (targetVisitedUrls) => {
+    const urls = targetVisitedUrls.targetsLog;
+    if (global._stats.targetsLogs.length <= global._config.maxTargetsLogLength) {
+      global._stats.targetsLogs.push(urls);
+    } else {
+      global._stats.targetsLogs.shift();
+      global._stats.targetsLogs.push(urls);
+    }
+    console.log(global._stats.targetsLogs);
+  });
+
   socket.on("disconnect", () => {
     global._stats.clientsCount -= 1;
+    global._stats.targetsLogs = [];
   });
 });
 
@@ -42,21 +55,23 @@ io.of("web").on("connection", (socket) => {
   });
 });
 
-setInterval(() => emitTargets(io), global._config.targetsIntervalTime);
+setInterval(() => emitTargets(io.of("client")), global._config.targetsIntervalTime);
 
 setInterval(() => emitStats(io.of("web")), global._config.statsIntervalTime);
 
 function emitTargets(socket) {
-  console.log("emit targets");
   socket.emit("targets", {
     currentTargets: global._stats.currentTargets,
   });
 }
 
 function emitStats(socket) {
+  const targetsLogs = [].concat.apply([], global._stats.targetsLogs);
+
   socket.emit("stats", {
     clientsCount: global._stats.clientsCount,
     webVisitorsCount: global._stats.webVisitorsCount,
+    targetsLogs
   });
 }
 
